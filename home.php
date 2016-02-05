0cf3d95514ba80c26e7063a7b71fef7b405db3cb
Logan Saruwatari <lsaruwatari@gmail.com>
2016-02-04 19:29:16 -0700

    added mysql query example in home.php for easy reference on making a simple query.

--- home.php
+++ home.php
@@ -1,12 +1,17 @@
 <?php
-include("db/connect.php");
+include("db/connect.php"); // this connects to the cmu_slider db so we can query it
 
-$sql = "SELECT * FROM users";
-$psql=$conn->prepare($sql);
-$psql->execute(array(":role"=>$nonsense));
-$row= $psql->fetchALL();
+$sql = "SELECT * FROM users"; // a string that is a mysql query
+$psql = $conn->prepare($sql); // prepare the mysql statement
+$psql->execute(); // execute the statement. if it is an insert then an associative array is used to avoid sql injection
+$row = $psql->fetchALL(); // fetchALL gets all results from query as an associative array.
+
 
 foreach($row as $user){
-	echo $user['username'];
+	echo "<h1>Username: " . $user['username'] . "</h1></br>"; // associative array access
+	echo "<h1>First Name: " . $user['f_name'] . "</h1></br>";
+	echo "<h1>Last Name: " . $user['l_name'] . "</h1></br>";
+	echo "<h1>Email: " . $user['email'] . "</h1></br>";
+
 }
 ?>
