ece4cb9cb1f10a0218a5a38a6981ba3c2044a079
Logan Saruwatari <lsaruwatari@gmail.com>
2016-02-16 15:01:02 -0700

    added slide editor to homepage as well as a javascript preview for what the slide will look like

--- home.php
+++ home.php
@@ -9,13 +9,44 @@ if(isset($_SESSION['username'])){
 ?>
 <!-- Main Content Begin -->
 <script src="//cdn.ckeditor.com/4.5.7/full/ckeditor.js"></script>
-<textarea name="editor1"></textarea>
-        <script>
-            CKEDITOR.replace( 'editor1' );
-        </script>
 
+<div class="container">
+	<form id="register_form">
+		
+		<div class="row">
+			<div class="twelve columns">
+				<label for="slide_title">Title</label>
+					<input type="text" name="slide_title" id="slide_title" class="u-full-width"/>
+			</div>
+		</div>
 
+		<div class="row">
+			<div class="twelve columns">
+				<label for="slide_content">Content</label>
+				<textarea name="slide_content" id="slide_content"></textarea>	
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="six columns">
+				<input type="button" value="Preview" class="button" id="preview_button"/>
+			</div>
+
+			<div class="six columns">
+				<input type="submit" value="Submit" class="button-primary" id="submit_slide_button"/>
+			</div>
+		</div>
+	
+	</form>
+
+	<div class="row slide_preview" id="slide_preview">
+
+	</div>
+</div>
+
+<!-- Main Content End -->
 <?php
+include("footer.php");
 
 } // endif
 
--- js/home.js
+++ js/home.js
@@ -1,5 +1,19 @@
-@require //cdn.ckeditor.com/4.5.7/full/ckeditor.js
 
 $(document).ready(function() {
-
+	CKEDITOR.replace('slide_content'); // replace textarea with nice editor
+	$("#preview_button").on("click", renderPreview);
 });
+
+
+function renderPreview(){
+
+	var title = $("#slide_title").val();
+	var content = CKEDITOR.instances['slide_content'].getData();	
+	var previewArea = $("#slide_preview");
+
+	console.log(title);
+	console.log(content);
+
+	previewArea.html("<h1 class='title_preview'>" + title + "</h1>");
+	previewArea.append("<p class='content_preview'>" + content + "</p>");
+}
