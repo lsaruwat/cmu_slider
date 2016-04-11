011364642e1c51c22c5340847b9a1735f5e361be
Logan Saruwatari <lsaruwatari@gmail.com>
2016-04-10 22:00:58 -0600

    made a custom slider that is kind of working. Added gitiginore rules to make comitting easier.

---
+++ customSlide.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>CSCI Slideshow</title>
+  <link rel="stylesheet" href="css/global.css">
+  <link rel="stylesheet" href="superslides-0.6.2/dist/stylesheets/superslides.css">
+  <link rel="stylesheet" href="css/customSlide.css">
+
+</head>
+<body>
+  <div id="slides">
+    <ul class="slides-container">
+    </ul>
+  </div>
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script src="superslides-0.6.2/examples/javascripts/jquery.easing.1.3.js"></script>
+  <script src="superslides-0.6.2/examples/javascripts/jquery.animate-enhanced.min.js"></script>
+  <script src="superslides-0.6.2/dist/jquery.superslides.js" type="text/javascript" charset="utf-8"></script>
+  <script>
+  window.addEventListener("load", ready);
+  var slideshow;
+  var slideNumber = 0;
+  function ready(){
+    setHeight();
+    initializeSlider();
+    setInterval(getNextSlide, 7000);
+  }
+
+  function setHeight(){
+    var height = window.innerHeight;
+    $(".slides-container").height(height-4);
+  }
+
+  function initializeSlider(){
+    for(var i=0; i<3; i++){
+
+      $.ajax({
+        url : "getSlide.php",
+        dataType : 'json',
+        type : 'POST',
+        data : {},
+        success : function (returnData) {
+          if(returnData.message == "Success"){
+            var nextSlideData = returnData.slide[0];
+            var imageTag = ""; //default image tag if there is none
+            var iframeTag = "";
+
+            if(nextSlideData.image !== ""){ //there is an image
+              imageTag = "<img src='" + nextSlideData.image + "'/>";
+
+            }
+
+            else if(nextSlideData.url !== ""){
+              iframeTag = "<iframe src='" + nextSlideData.url + "' ></iframe>";
+            }
+
+            else if (nextSlideData.url === "" && nextSlideData.image === "")imageTag="<img src='http://www.coloradomesa.edu//_files/images/news/campus.jpg'/>";
+            var nextSlide = "<li>" + imageTag + iframeTag + "<div class='content_container'><h1>" + nextSlideData.title + "</h1><p>" + nextSlideData.content + "</p></div></li>";
+            $("ul.slides-container").append(nextSlide);
+            $("li")[0].setAttribute("class", "prev_slide");
+            $("li")[1].setAttribute("class", "active_slide");
+            $("li")[2].setAttribute("class", "next_slide");
+          }
+        }
+
+      });
+    }
+  }
+
+  function getNextSlide(){
+
+    $.ajax({
+      url : "getSlide.php",
+      dataType : 'json',
+      type : 'POST',
+      async : false,
+      data : {},
+
+      success : function (returnData) {
+        if(returnData.message == "Success"){
+          //console.log(returnData);
+          var nextSlideData = returnData.slide[0];
+
+
+          var imageTag = ""; //default image tag if there is none
+          var iframeTag = "";
+
+          if(nextSlideData.image !== ""){ //there is an image
+            imageTag = "<img src='" + nextSlideData.image + "'/>";
+
+          }
+
+
+          else if(nextSlideData.url !== ""){
+            iframeTag = "<iframe src='" + nextSlideData.url + "' ></iframe>";
+          }
+
+          else if (nextSlideData.url === "" && nextSlideData.image === "")imageTag="<img src='http://www.coloradomesa.edu//_files/images/news/campus.jpg'/>";
+          var ajaxSlide =  "<li class='next_slide'>" + imageTag + iframeTag + "<div class='content_container'><h1>" + nextSlideData.title + "</h1><p>" + nextSlideData.content + "</p></div></li>";
+          
+          var slidesContainer = $("ul.slides-container");
+
+          var prevSlide = $("ul.slides-container li")[0];
+          var activeSlide = $("ul.slides-container li")[1];
+          var nextSlide = $("ul.slides-container li")[2];
+
+          console.log(prevSlide);
+          console.log(activeSlide);
+          console.log(nextSlide);
+
+          prevSlide.remove();
+          activeSlide.setAttribute("class","prev_slide");
+          nextSlide.setAttribute("class","active_slide");
+          slidesContainer.append(ajaxSlide);
+
+
+        }
+
+        else {
+          console.log(returnData);
+        }
+
+      }
+
+    });
+
+  }
+
+
+
+  </script>
+</body>
+</html>
--- edit_slides.php
+++ edit_slides.php
@@ -8,6 +8,13 @@ include("header.php");
 ?>
 
 <div class="container">
+<div class="row table-titles">
+	<div class='one columns'><h3>Id</h3></div>
+	<div class='two columns'><h3>Delete</h3></div>
+	<div class='two columns'><h3>Title</h3></div>
+	<div class='two columns'><h3>Content</h3></div>
+	<div class='five columns'><h3>Image/Url</h3></div>
+</div>
 <?php
 
 if($_SESSION['permissions'] === 'admin'){
@@ -19,7 +26,7 @@ if($_SESSION['permissions'] === 'admin'){
 	$rows = $psql->fetchAll();
 
 	foreach($rows as $row){
-		echo "<div class='edit-slide row'><div class='one columns'>" . $row['id'] . "</div><div class='three columns'><input type='button' id='' onclick='deleteSlideById(" . $row['id'] . ")' value='Delete'/></div><div class='two columns'> " . $row['title'] . "</div><div class='two columns'> " . $row['content'] . "</div><div class='two columns'> " . $row['image'] . "</div><div class='two columns'> " . $row['url'] . "</div></div>";
+		echo "<div class='edit-slide row'><div class='one columns'>" . $row['id'] . "</div><div class='two columns'><input type='button' id='' onclick='deleteSlideById(" . $row['id'] . ")' value='Delete'/></div><div class='two columns'> " . $row['title'] . "</div><div class='two columns'> " . $row['content'] . "</div><div class='two columns'> " . $row['image'] . "</div><div class='three columns'> " . $row['url'] . "</div></div>";
 	}
 include("footer.php");
 ?>
