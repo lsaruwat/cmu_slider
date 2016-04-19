178e815cbd2eeef1b6c22712f0bb061e165a34a9
Logan Saruwatari <lsaruwatari@gmail.com>
2016-04-18 20:56:44 -0600

    changed logging system to create new logfile each day if there is an error. Also changed gitignore to ignore log files

--- logError.php
+++ logError.php
@@ -1,23 +1,20 @@
 <?php
+session_start();
+
+$_SESSION['errorUrl'] = $_POST['url'];
 
 $url = $_POST['url'];
-$message = "Failed";
-
 
 date_default_timezone_set('America/Denver');
-$date = date('m/d/Y h:i:s a', time());
+$dateTime = date('m/d/Y h:i:s a', time());
+$date = date('m-d-Y', time());
 
-$entry = $url . " - " . $date . "\n";
-$file = 'logs/log.txt';
+$entry = $url . " - " . $dateTime . "\n";
+$file = "logs/log-" . $date . ".txt"; //make a new log file for each day
 // Write the contents to the file, 
 // using the FILE_APPEND flag to append the content to the end of the file
 // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
 file_put_contents($file, $entry, FILE_APPEND | LOCK_EX);
 
-
-
-$message = "Success";
-
-//echo $_POST;
 ?>
 
