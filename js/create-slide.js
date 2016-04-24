6c96056bde97e4be8d6f734c92be1bae087c78f1
Logan Saruwatari <lsaruwatari@gmail.com>
2016-04-24 01:12:33 -0600

    added support for iframe preview as well as displaying loading message while waiting for load event of iframe. Want to add spinner style animation

--- js/create-slide.js
+++ js/create-slide.js
@@ -12,11 +12,33 @@ function renderPreview(){
 
 	var title = $("#slide_title").val();
 	var content = CKEDITOR.instances['slide_content'].getData();
+	var url = $("#slide_url").val();
 	var contentContainer = $("#content_container");
 	var slidePreview = $("#slide_preview");
 	var height = window.innerHeight;
 
-	slidePreview.attr("style", "display: inline-block; height:" + height + "px; background-image: url('" + imageUrl + "');" );
+	if(url == ""){
+		slidePreview.attr("style", "display: inline-block; height:" + height + "px; background-image: url('" + imageUrl + "');" );
+		console.log(url);
+	}
+
+	else{
+		console.log(url);
+		slidePreview.attr("style", "display: inline-block; height:" + height + "px;" );
+		slidePreview.append("<h1 id='loading'>Loading Please Wait...</h1>");
+		$('<iframe/>')
+		  .attr('id', 'previewIframe')
+		  .attr('src', url)
+		  .attr("style", "display: none; overflow: hidden;")
+		  .appendTo(slidePreview)
+		  .load(function() {
+		  	$("#loading").attr("style", "display:none;");
+		  	$(this).attr("style","display: inline-block;");
+		 });
+
+		//slidePreview.append("<iframe src=" + url + "></iframe>");
+	}
+
 	contentContainer.html("<h1 class='title_preview'>" + title + "</h1>");
 	contentContainer.append("<p class='content_preview'>" + content + "</p>");
 
