a9cb931a73447aba958f03652e17ae168886281e
Kalvin <Ultima_112@yahoo.com>
2016-04-28 08:45:28 -0600

    Restricted users table to superuser only

--- users.php
+++ users.php
@@ -14,7 +14,7 @@ if(isset($_SESSION['username'])){
 	$group = $group_statement->fetchColumn(0);
 	$group_statement->closeCursor();
 	
-	if($group === 'admin' || $group === "superuser") {
+	if($group === "superuser") {
 		$users_query = "SELECT *
 		FROM Users
 		ORDER BY username";
